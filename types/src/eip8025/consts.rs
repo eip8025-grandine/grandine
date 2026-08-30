293e8e9d1865fe87a3ece573d83c8e180031e753
keshavsharma25 <skeshav0825@gmail.com>
2026-08-30 17:13:32 +0530

    feat: Add DOMAIN_EXECUTION_PROOF

--- types/src/eip8025/consts.rs
+++ types/src/eip8025/consts.rs
@@ -6,9 +6,11 @@
 //!
 //! [EIP-8025 consensus spec]: https://github.com/ethereum/consensus-specs/blob/7fa044833194cbea2908f76c0de102d168d88fb0/specs/_features/eip8025/beacon-chain.md
 
+use hex_literal::hex;
 use typenum::Unsigned as _;
 
 use crate::eip8025::primitives::MaxProofSize;
+use crate::phase0::primitives::{DomainType, H32};
 
 /// The maximum length of
 /// [`ProofData`](crate::eip8025::containers::ProofData): 4 MiB.
@@ -39,3 +41,6 @@ pub const STATELESS_INPUT_SCHEMA_ID: u16 = 0x1501;
 /// Each fixed-size portion includes the SSZ offset for its
 /// variable-size field.
 pub const MAX_SIGNED_EXECUTION_PROOF_ENVELOPE_SIZE: usize = 108 + 37 + MAX_PROOF_SIZE;
+
+/// The domain used for signing execution proofs.
+pub const DOMAIN_EXECUTION_PROOF: DomainType = H32(hex!("0F000000"));
